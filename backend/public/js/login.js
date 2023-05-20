c4d35de8bf6be4e35090060c3122773164ce035b
Ömer <omerkibar@marun.edu.tr>
2023-05-20 23:59:47 +0300

    pardon

--- backend/public/js/login.js
+++ backend/public/js/login.js
@@ -7,8 +7,8 @@ async function login(){
           'Cookie' : 'access-token='
         },
         body: `{
-            "username":"${document.getElementsByName('email')[0].value}",
-            "content":"${document.getElementsByName('psw')[0].value}"}`,
+            "email":"${document.getElementsByName('email')[0].value}",
+            "password":"${document.getElementsByName('psw')[0].value}"}`,
         });
         
     response.json().then(data => {
