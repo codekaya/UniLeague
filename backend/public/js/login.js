cec1553c20ecc6792a06cfa684cfd2bfeb6046b3
Ömer <omerkibar@marun.edu.tr>
2023-05-23 11:02:30 +0300

    login page bağlandı

--- backend/public/js/login.js
+++ backend/public/js/login.js
@@ -1,4 +1,6 @@
-async function login(){
+async function login(event){
+    event.preventDefault();
+    const formData = new FormData(document.getElementById('form'))
     const response = await fetch("http://localhost:5000/user/login", {
         method: 'POST',
         headers: {
@@ -7,13 +9,19 @@ async function login(){
           'Cookie' : 'access-token='
         },
         body: `{
-            "email":"${document.getElementsByName('email')[0].value}",
-            "password":"${document.getElementsByName('psw')[0].value}"}`,
+            "email":"${formData.get('email')}",
+            "password":"${formData.get('psw')}"}`,
         });
         
     response.json().then(data => {
         if(data.success){
             window.location.href = '/'
         }
+        else{
+            document.getElementById('errorMessage').style.display = "block"
+        }
     });
 }
+
+const form = document.getElementById("form");
+form.addEventListener("submit", login);
